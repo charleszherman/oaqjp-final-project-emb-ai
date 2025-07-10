e852154320f026ff63d3ee2b2be5389f4b673456
charleszherman <charleszherman@yahoo.com>
2025-07-10 19:02:12 -0400

    full output

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from emotion_detection.emotion_detection import emotion_detector
-
+import json
 
 app = Flask("emotionDetector")
 
@@ -15,7 +15,12 @@ def emotion_detector_caller():
     text_to_analyze = request.args["textToAnalyze"]
     result = emotion_detector(text_to_analyze)
 
-    return result
+       
+    result_str = json.dumps(result)
+    
+    formatted_result = "For the given statement, the system response is " + result_str + " The dominant emotion is " + result["dominant_emotion"]
+    
+    return formatted_result
 
 if __name__ == "__main__":
     app.run(debug = True)
